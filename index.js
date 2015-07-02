b50a7caaa45371e7ccbae153e4927890f0ec9f6b
Howard Wu <howardwu@berkeley.edu>
2015-07-02 16:22:30 -0700

    Added all sections for testing, converted fillTestInstance to run synchronously for request limits

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var testInstanceCreator = require('./test-instance.js');
+var async = require('async');
 
-//abstract common blockchain adapters
+/***** API Providers for abstract-common-blockchain *****/
 var biteasy = require("biteasy-unofficial");
 var blockchaininfo = require("blockchaininfo-unofficial");
 var blockcypher = require("blockcypher-unofficial");
@@ -8,6 +9,7 @@ var blockr = require("blockr-unofficial");
 var blocktrail = require("blocktrail-unofficial");
 var chain = require("chain-unofficial");
 
+/***** Biteasy *****/
 var biteasyMainnet = biteasy({
   network: "mainnet"
 });
@@ -15,8 +17,10 @@ var biteasyTestnet = biteasy({
   network: "testnet"
 });
 
+/***** Blockchain.info *****/
 var blockchaininfoMainnet = blockchaininfo(); 
 
+/***** Blockcypher *****/
 var blockcypherMainnet = blockcypher({
   network: "mainnet"
 });
@@ -24,8 +28,10 @@ var blockcypherTestnet = blockcypher({
   network: "testnet"
 });
 
+/***** Blockr.io *****/
 var blockrMainnet = blockr();
 
+/***** Blocktrail *****/
 var blocktrailMainnet = blocktrail({
   network: "mainnet",
   apiKey: process.env.BLOCKTRAIL_API_KEY_ID,
@@ -37,6 +43,7 @@ var blocktrailTestnet = blocktrail({
   apiSecret: process.env.BLOCKTRAIL_API_KEY_SECRET
 });
 
+/***** Chain *****/
 var chainMainnet = chain({
   network: "testnet", 
   key: process.env.CHAIN_API_KEY_ID, 
@@ -48,8 +55,7 @@ var chainTestnet = chain({
   secret: process.env.CHAIN_API_KEY_SECRET
 });
 
-
-
+/** Matrix of API Providers **/
 var testMatrix = [
   {
     name: "biteasyMainnet",
@@ -113,7 +119,9 @@ var testMatrix = [
   }
 ];
 
-
+/* Fills in our testInstance object with the results from testing 
+ * the API provider. Because of query limits on certain providers,
+ * tests are run with 0.5 second timeouts to prevent errors. */
 function fillTestInstance(apiProvider, callback) {
   var count = 0;
   var testInstance = apiProvider.test;
@@ -121,113 +129,245 @@ function fillTestInstance(apiProvider, callback) {
   var blockId = (apiProvider.network === "mainnet") ? "00000000000000000a6e36f5f310f7c01a58a1c3d3f2dd63873379e03a937424" : "0000000002d9daeab13dd9bf375bb6851824f39b95da84b4f024a278b0164ac6";
   var txid = (apiProvider.network === "mainnet") ? "c3e5f13833ab30fdeff02582db0ecba59a35c3512f6fe3fa0326aebafafa49a0" : "bbca54add4a55dd6086df741e3f89288f290a94d69ebb932a140262450b2ebcf";
 
+  async.series([
+    function (callback) { 
+      apiProvider.client.Addresses.Summary([address], function (err, resp) {
+        if (!err) {
+          testInstance.Addresses.Summary.address = (resp[0].address != null);
+          testInstance.Addresses.Summary.balance = (resp[0].balance != null);
+          testInstance.Addresses.Summary.totalReceived = (resp[0].totalReceived != null);
+          testInstance.Addresses.Summary.totalSent = (resp[0].totalSent != null);
+          testInstance.Addresses.Summary.txCount = (resp[0].txCount != null);
+          callback(null, testInstance.Addresses.Summary);
+        }
+        else {
+          callback('Addresses.Summary: ' + err, null);
+        }
+      });
+    },
 
-  apiProvider.client.Addresses.Summary([address], function (err, resp){
-    if (!err) {
-      testInstance.address.address = (resp[0].address != null);
-      testInstance.address.balance = (resp[0].balance != null);
-      testInstance.address.totalReceived = (resp[0].totalReceived != null);
-      testInstance.address.totalSent = (resp[0].totalSent != null);
-      testInstance.address.txCount = (resp[0].txCount != null);
-    }
-    if (++ count === 4){
-      callback();
-    }
-  });
+    function (callback) {
+      apiProvider.client.Addresses.Transactions([address], function (err, resp) {
+        if (!err) {
+          resp = resp[0][0];
+          testInstance.Addresses.Transactions.blockHeight = (resp.blockHeight != null);
+          testInstance.Addresses.Transactions.blockId = (resp.blockId != null);
+          testInstance.Addresses.Transactions.hex = (resp.hex != null);
+          testInstance.Addresses.Transactions.txid = (resp.txid != null);
+          testInstance.Addresses.Transactions.txId = (resp.txId != null);
+          callback(null, testInstance.Addresses.Transactions);
+        }
+        else {
+          callback('Addresses.Transactions: ' + err, null);
+        }
+      });
+    },
 
-  apiProvider.client.Addresses.Unspents([address], function (err, resp){
-    if (!err) {
-      var unspentObj = resp[0][0];
-      testInstance.unspent.txid = (unspentObj.txid != null);
-      testInstance.unspent.txId = (unspentObj.txId != null);
-      testInstance.unspent.address = (unspentObj.address != null);
-      testInstance.unspent.scriptPubKey = (unspentObj.scriptPubKey != null);
-      testInstance.unspent.amount = (unspentObj.amount != null);
-      testInstance.unspent.value = (unspentObj.value != null);
-      testInstance.unspent.confirmations = (unspentObj.confirmations != null);
-    }
-    if (++ count === 4){
-      callback();
-    }
-  });
+    function (callback) {
+      apiProvider.client.Addresses.Unspents([address], function (err, resp) {
+        if (!err) {
+          var unspentObj = resp[0][0];
+          testInstance.Addresses.Unspents.address = (unspentObj.address != null);
+          testInstance.Addresses.Unspents.confirmations = (unspentObj.confirmations != null);
+          testInstance.Addresses.Unspents.txid = (unspentObj.txid != null);
+          testInstance.Addresses.Unspents.txId = (unspentObj.txId != null);
+          testInstance.Addresses.Unspents.value = (unspentObj.value != null);
+          testInstance.Addresses.Unspents.amount = (unspentObj.amount != null);
+          testInstance.Addresses.Unspents.scriptPubKey = (unspentObj.scriptPubKey != null);
+          callback(null, testInstance.Addresses.Unspents);
+        }
+        else {
+          callback('Addresses.Unspents: ' + err, null);
+        }
+      });
+    },
 
-  apiProvider.client.Transactions.Get([txid], function (err, resp){
-    if (!err) {
-      var transactionObj = resp[0];
-      testInstance.transaction.hex = (transactionObj.hex != null);
-      testInstance.transaction.txHex = (transactionObj.txHex != null);
-      testInstance.transaction.txid = (transactionObj.txid != null);
-      testInstance.transaction.txId = (transactionObj.txId != null);
-      testInstance.transaction.version = (transactionObj.version != null);
-      testInstance.transaction.locktime = (transactionObj.locktime != null);
-      testInstance.transaction.fee = (transactionObj.fee != null);
+    function (callback) {
+      apiProvider.client.Blocks.Get([blockId], function (err, resp) {
+        if (!err) {
+          var blockObj = resp[0];
+          testInstance.Blocks.Get.blockHex = (blockObj.blockHex != null);
+          testInstance.Blocks.Get.blockId = (blockObj.blockId != null);
+          callback(null, testInstance.Blocks.Get);
+        }
+        else {
+          callback('Blocks.Get: ' + err, null);
+        }
+      });
+    },
 
-      testInstance.transaction.vin.txid = (transactionObj.vin[0].txid != null);
-      testInstance.transaction.vin.txId = (transactionObj.vin[0].txId != null);
-      testInstance.transaction.vin.vout = (transactionObj.vin[0].vout != null);
-      testInstance.transaction.vin.scriptSig.asm = (transactionObj.vin[0].scriptSig.asm != null);
-      testInstance.transaction.vin.scriptSig.hex = (transactionObj.vin[0].scriptSig.hex != null);
-      testInstance.transaction.vin.sequence = (transactionObj.vin[0].sequence != null);
+    function (callback) {
+      apiProvider.client.Blocks.Latest(function (err, resp) {
+        if (!err) {
+          var blockObj = resp;
+          testInstance.Blocks.Latest.blockHex = (blockObj.blockHex != null);
+          testInstance.Blocks.Latest.blockId = (blockObj.blockId != null);
+          callback(null, testInstance.Blocks.Latest);
+        }
+        else {
+          callback('Blocks.Latest: ' + err, null);
+        }
+      });
+    },
 
-      testInstance.transaction.vout.value = (transactionObj.vout[0].value != null);
-      testInstance.transaction.vout.index = (transactionObj.vout[0].index  != null);
-      testInstance.transaction.vout.n = (transactionObj.vout[0].n != null);
-      testInstance.transaction.vout.spentTxid = (transactionObj.vout[0].spentTxid != null);
-      testInstance.transaction.vout.scriptPubKey.asm = (transactionObj.vout[0].scriptPubKey.asm != null);
-      testInstance.transaction.vout.scriptPubKey.hex = (transactionObj.vout[0].scriptPubKey.hex != null);
-      testInstance.transaction.vout.scriptPubKey.reqSigs = (transactionObj.vout[0].scriptPubKey.reqSigs != null);
-      testInstance.transaction.vout.scriptPubKey.type = (transactionObj.vout[0].scriptPubKey.type != null);
-      testInstance.transaction.vout.addresses = (transactionObj.vout[0].addresses != null);
+    function (callback) {
+      apiProvider.client.Blocks.Transactions([blockId], function (err, resp) {
+        if (!err) {
+          var blockObj = resp[0][0];
+          testInstance.Blocks.Transactions.blockId = (blockObj.blockId != null);
+          testInstance.Blocks.Transactions.txid = (blockObj.txid != null);
+          testInstance.Blocks.Transactions.txId = (blockObj.txId != null);
+          callback(null, testInstance.Blocks.Transactions);
+        }
+        else {
+          callback('Blocks.Transactions: ' + err, null);
+        }
+      });
+    },
 
-      testInstance.transaction.confirmations = (transactionObj.confirmations != null);
-      testInstance.transaction.blocktime = (transactionObj.blocktime != null);
-      testInstance.transaction.blockhash = (transactionObj.blockhash != null);
-      testInstance.transaction.blockindex = (transactionObj.blockindex != null);
-      testInstance.transaction.timeReceived = (transactionObj.timeReceived != null);
-    }
-    if (++ count === 4){
-      callback();
-    }
-  });
+    function (callback) {
+      apiProvider.client.Transactions.Get([txid], function (err, resp) {
+        if (!err) {
+          var transactionObj = resp[0];
+          testInstance.Transactions.Get.hex = (transactionObj.hex != null);
+          testInstance.Transactions.Get.txHex = (transactionObj.txHex != null);
+          testInstance.Transactions.Get.txid = (transactionObj.txid != null);
+          testInstance.Transactions.Get.txId = (transactionObj.txId != null);
+          testInstance.Transactions.Get.version = (transactionObj.version != null);
+          testInstance.Transactions.Get.locktime = (transactionObj.locktime != null);
+          testInstance.Transactions.Get.fee = (transactionObj.fee != null);
 
-  apiProvider.client.Blocks.Get([blockId], function (err, resp){
-    if (!err) {
-      var blockObj = resp[0];
-      testInstance.block.blockHex = (blockObj.blockHex != null);
-      testInstance.block.blockId = (blockObj.blockId != null);
+          testInstance.Transactions.Get.vin.txid = (transactionObj.vin[0].txid != null);
+          testInstance.Transactions.Get.vin.txId = (transactionObj.vin[0].txId != null);
+          testInstance.Transactions.Get.vin.vout = (transactionObj.vin[0].vout != null);
+          testInstance.Transactions.Get.vin.scriptSig.asm = (transactionObj.vin[0].scriptSig.asm != null);
+          testInstance.Transactions.Get.vin.scriptSig.hex = (transactionObj.vin[0].scriptSig.hex != null);
+          testInstance.Transactions.Get.vin.sequence = (transactionObj.vin[0].sequence != null);
+
+          testInstance.Transactions.Get.vout.value = (transactionObj.vout[0].value != null);
+          testInstance.Transactions.Get.vout.index = (transactionObj.vout[0].index  != null);
+          testInstance.Transactions.Get.vout.n = (transactionObj.vout[0].n != null);
+          testInstance.Transactions.Get.vout.spentTxid = (transactionObj.vout[0].spentTxid != null);
+          testInstance.Transactions.Get.vout.scriptPubKey.asm = (transactionObj.vout[0].scriptPubKey.asm != null);
+          testInstance.Transactions.Get.vout.scriptPubKey.hex = (transactionObj.vout[0].scriptPubKey.hex != null);
+          testInstance.Transactions.Get.vout.scriptPubKey.reqSigs = (transactionObj.vout[0].scriptPubKey.reqSigs != null);
+          testInstance.Transactions.Get.vout.scriptPubKey.type = (transactionObj.vout[0].scriptPubKey.type != null);
+          testInstance.Transactions.Get.vout.addresses = (transactionObj.vout[0].addresses != null);
+
+          testInstance.Transactions.Get.confirmations = (transactionObj.confirmations != null);
+          testInstance.Transactions.Get.blocktime = (transactionObj.blocktime != null);
+          testInstance.Transactions.Get.blockhash = (transactionObj.blockhash != null);
+          testInstance.Transactions.Get.blockindex = (transactionObj.blockindex != null);
+          testInstance.Transactions.Get.timeReceived = (transactionObj.timeReceived != null);
+
+          callback(null, testInstance.Transactions.Get);
+        }
+        else {
+          callback('Transactions.Get: ' + err, null);
+        }
+      });
+    },
+
+    function (callback) {
+      apiProvider.client.Transactions.Latest(function (err, resp) {
+        if (!err) {
+          var transactionObj = resp[0];
+          testInstance.Transactions.Latest.hex = (transactionObj.hex != null);
+          testInstance.Transactions.Latest.txHex = (transactionObj.txHex != null);
+          testInstance.Transactions.Latest.txid = (transactionObj.txid != null);
+          testInstance.Transactions.Latest.txId = (transactionObj.txId != null);
+          testInstance.Transactions.Latest.version = (transactionObj.version != null);
+          testInstance.Transactions.Latest.locktime = (transactionObj.locktime != null);
+          testInstance.Transactions.Latest.fee = (transactionObj.fee != null);
+
+          testInstance.Transactions.Latest.vin.txid = (transactionObj.vin[0].txid != null);
+          testInstance.Transactions.Latest.vin.txId = (transactionObj.vin[0].txId != null);
+          testInstance.Transactions.Latest.vin.vout = (transactionObj.vin[0].vout != null);
+          testInstance.Transactions.Latest.vin.scriptSig.asm = (transactionObj.vin[0].scriptSig.asm != null);
+          testInstance.Transactions.Latest.vin.scriptSig.hex = (transactionObj.vin[0].scriptSig.hex != null);
+          testInstance.Transactions.Latest.vin.sequence = (transactionObj.vin[0].sequence != null);
+
+          testInstance.Transactions.Latest.vout.value = (transactionObj.vout[0].value != null);
+          testInstance.Transactions.Latest.vout.index = (transactionObj.vout[0].index  != null);
+          testInstance.Transactions.Latest.vout.n = (transactionObj.vout[0].n != null);
+          testInstance.Transactions.Latest.vout.spentTxid = (transactionObj.vout[0].spentTxid != null);
+          testInstance.Transactions.Latest.vout.scriptPubKey.asm = (transactionObj.vout[0].scriptPubKey.asm != null);
+          testInstance.Transactions.Latest.vout.scriptPubKey.hex = (transactionObj.vout[0].scriptPubKey.hex != null);
+          testInstance.Transactions.Latest.vout.scriptPubKey.reqSigs = (transactionObj.vout[0].scriptPubKey.reqSigs != null);
+          testInstance.Transactions.Latest.vout.scriptPubKey.type = (transactionObj.vout[0].scriptPubKey.type != null);
+          testInstance.Transactions.Latest.vout.addresses = (transactionObj.vout[0].addresses != null);
+
+          testInstance.Transactions.Latest.confirmations = (transactionObj.confirmations != null);
+          testInstance.Transactions.Latest.blocktime = (transactionObj.blocktime != null);
+          testInstance.Transactions.Latest.blockhash = (transactionObj.blockhash != null);
+          testInstance.Transactions.Latest.blockindex = (transactionObj.blockindex != null);
+          testInstance.Transactions.Latest.timeReceived = (transactionObj.timeReceived != null);
+
+          callback(null, testInstance.Transactions.Latest);
+        }
+        else {
+          callback('Transactions.Latest: ' + err, null);
+        }
+      });
+    },
+
+    function (callback) {
+      apiProvider.client.Transactions.Outputs([
+        {
+          txid: txid,
+          vout: 0
+        }
+      ], function (err, resp) {
+        if (!err) {
+          var blockObj = resp[0];
+          testInstance.Transactions.Outputs.scriptPubKey = (blockObj.scriptPubKey != null);
+          testInstance.Transactions.Outputs.txid = (blockObj.txid != null);
+          testInstance.Transactions.Outputs.txId = (blockObj.txId != null);
+          testInstance.Transactions.Outputs.value = (blockObj.value != null);
+          testInstance.Transactions.Outputs.vout = (blockObj.vout != null);
+          callback(null, testInstance.Transactions.Outputs);
+        }
+        else {
+          callback('Transactions.Outputs: ' + err, null);
+        }
+      });
+    },
+
+    function (callback) {
+      apiProvider.client.Transactions.Status([txid], function (err, resp) {
+        if (!err) {
+          var blockObj = resp[0];
+          testInstance.Transactions.Status.blockId = (blockObj.blockId != null);
+          testInstance.Transactions.Status.txid = (blockObj.txid != null);
+          testInstance.Transactions.Status.txId = (blockObj.txId != null);
+          callback(null, testInstance.Transactions.Status);
+        }
+        else {
+          callback('Transactions.Status: ' + err, null);
+        }
+      });
     }
-    if (++ count === 4){
-      callback();
+  ], function (error, results) {
+    if (error) {
+      console.log(error);
     }
+    callback();
   });
 }
 
-function populateMatrix(callback){
+function populateMatrix(callback) {
   var apiCount = 0;
-  testMatrix.forEach(function (apiProvider){
-    fillTestInstance(apiProvider, function(){
+  testMatrix.forEach(function (apiProvider) {
+    fillTestInstance(apiProvider, function () {
       if(++apiCount === testMatrix.length){
-        callback(testMatrix);
+        callback(JSON.stringify(testMatrix));
       }
     });
   });
 }
 
-
-populateMatrix(function(testMatrix){
-  console.log(JSON.stringify(testMatrix));
+populateMatrix(function (testMatrix) {
+  console.log(testMatrix);
 });
 
-
 module.exports = populateMatrix;
-
-
-
-
-
-
-
-
-
-
-
--- test-instance.js
+++ test-instance.js
@@ -1,72 +1,155 @@
-var createTestInstance = function() {
-  var Address = {
-    address: false,
-    balance: false,
-    totalReceived: false,
-    totalSent: false,
-    txCount: false
-  };
+var createTestInstance = function () {
+  
+  var Addresses = {
+    Summary: {
+      address: false,
+      balance: false,
+      totalReceived: false,
+      totalSent: false,
+      txCount: false
+    },
 
-  var Unspent = {
-    txId: false,
-    txid: false,
-    vout: false,
-    address: false,
-    scriptPubKey: false,
-    amount: false,
-    value: false,
-    confirmations: false
-  };
+    Transactions: {
+      blockHeight: false,
+      blockId: false,
+      hex: false,
+      txHex: false,
+      txid: false,
+      txId: false
+    },
 
-  var Transaction= {
-    hex: false,
-    txHex: false,
-    txid: false,
-    txId: false,
-    version: false,
-    locktime: false,
-    fee: false,
-    vin: {
+    Unspents: {
+      address: false,
+      confirmations: false,
       txid: false,
       txId: false,
+      value: false,
+      amount: false,
       vout: false,
-      scriptSig: {
+      scriptPubKey: false
+    }
+  };
+
+  var Blocks = {
+    Get: {
+      blockHex: false,
+      blockId: false
+    },
+
+    Latest: {
+      blockHex: false,
+      blockId: false
+    },
+
+    Propagate: {},
+
+    Transactions: {
+      blockId: false,
+      txid: false,
+      txId: false
+    }
+  };
+
+  var Transactions = {
+    Get: {
+      hex: false,
+      txHex: false,
+      txid: false,
+      txId: false,
+      version: false,
+      locktime: false,
+      fee: false,
+      vin: {
+        txid: false,
+        txId: false,
+        vout: false,
+        scriptSig: {
           asm: false,
           hex: false
+        },
+        sequence: false
       },
-      sequence: false
+      vout: {
+        value: false,
+        index: false,
+        n: false,
+        spentTxid: false,
+        scriptPubKey: {
+          asm: false,
+          hex: false,
+          reqSigs: false,
+          type: false,
+          addresses: false
+        }
+      },
+      blocktime: false,
+      blockhash: false,
+      blockindex: false,
+      confirmations: false,
+      timeReceived: false
     },
-    vout: {
+
+    Latest: {
+      hex: false,
+      txHex: false,
+      txid: false,
+      txId: false,
+      version: false,
+      locktime: false,
+      fee: false,
+      vin: {
+        txid: false,
+        txId: false,
+        vout: false,
+        scriptSig: {
+          asm: false,
+          hex: false
+        },
+        sequence: false
+      },
+      vout: {
+        value: false,
+        index: false,
+        n: false,
+        spentTxid: false,
+        scriptPubKey: {
+          asm: false,
+          hex: false,
+          reqSigs: false,
+          type: false,
+          addresses: false
+        }
+      },
+      blocktime: false,
+      blockhash: false,
+      blockindex: false,
+      confirmations: false,
+      timeReceived: false
+    },
+
+    Outputs: {
+      scriptPubKey: false,
+      txid: false,
+      txId: false,
       value: false,
-      index: false,
-      n: false,
-      spentTxid: false,
-      scriptPubKey: {
-        asm: false,
-        hex: false,
-        reqSigs: false,
-        type: false,
-        addresses: false
-      }
+      vout: false
     },
-    confirmations: false,
-    blocktime: false,
-    blockhash: false,
-    blockindex: false,
-    timeReceived: false
+
+    Propagate: {
+
+    },
+
+    Status: {
+      blockId: false,
+      txid: false,
+      txId: false
+    }
   };
 
-  var Block = {
-    blockHex: false,
-    blockId: false
-  };
-
-
-  return({
-    address: Address,
-    block: Block,
-    transaction: Transaction,
-    unspent: Unspent
+  return ({
+    Addresses: Addresses,
+    Blocks: Blocks,
+    Transactions: Transactions
   });
 }
 
